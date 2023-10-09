f22650f7a1aa2ef1f4eb0f4227d9e96ac69785a7
Pam Lahoud <32913866+bluefooted@users.noreply.github.com>
2023-10-08 18:54:14 -0700

    Finishing chapter 9

--- ch9/02. Query Plan Analyzer.sql
+++ ch9/02. Query Plan Analyzer.sql
@@ -11,3 +11,17 @@ INNER JOIN Sales.OrderTracking AS ot ON ot.SalesOrderID = soh.SalesOrderID
 WHERE ot.EventDateTime >= @UpdatedOn;
 GO
 
+CREATE OR ALTER PROCEDURE usp_SalesTracking_Test @UpdatedOn datetime
+AS 
+SET NOCOUNT ON;
+SELECT *
+FROM Sales.SalesOrderHeader AS soh 
+INNER JOIN Sales.OrderTracking AS ot ON ot.SalesOrderID = soh.SalesOrderID
+WHERE ot.EventDateTime >= @UpdatedOn;
+GO
+
+EXECUTE usp_SalesTracking_Test '2014-6-30 00:00:00'
+GO
+
+ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = OFF;
+DBCC TRACEON (4136, -1);
